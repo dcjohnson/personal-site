153ca4df7b83bda78bf5758f5b8dda0e3ab962ff
Dylan Conrad Johnson <djohnson@ancestry.com>
2015-06-06 01:50:46 -0600

    inital
    trying stuff out.

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+	fs.readFile('./html/index.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('{html:' + data + '}');
+		res.end();
+	});
+});
+
+server.listen(8000);

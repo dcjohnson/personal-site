9f74dc8078b1d8754b960eb023ff51ddfbad0477
Dylan Conrad Johnson <djohnson@ancestry.com>
2015-07-03 16:05:40 -0600

    steam

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-	fs.readFile('./html/index.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		res.end();
-	});
+    var stream = fs.createReadStream(__dirname + '/html/index.html');
+    stream.pipe(res);
 });
 
 server.listen(8000);

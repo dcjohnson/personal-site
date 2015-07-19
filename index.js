1a0fdbf052bd1b11dbdbef85124ca932eeccfafb
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-07-19 14:33:11 -0600

    using my custom libraries.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('tiny-url');
 
-var server = http.createServer(function(req, res) {
+var route = new url.Router();
+
+route.get('/[ac]/wat', function(req, res) {
+    res.write('this is a loop');
+    res.end();
+});
+
+route.get('/(e|3)/wat', function(req, res) {
+    res.write('this is a enum');
+    res.end();
+})
+
+route.get('/test/wat', function(req, res) {
+    res.write('wat');
+    res.end();
+});
+
+route.get('/', function(req, res) {
     var stream = fs.createReadStream(__dirname + '/html/index.html');
     stream.pipe(res);
+});
+
+route.page404 = function(req, res) {
+    res.write('I have overridden the page.');
+    res.end();
+}
+
+var server = http.createServer(function(req, res) {
+    route.apply(req, res);
 });
 
 server.listen(8000);
